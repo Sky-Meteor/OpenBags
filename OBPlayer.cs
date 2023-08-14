e5a8ac8cba76b88b468fc19f6ad0f5e9cddc0417
Furgo <furgos@qq.com>
2023-08-14 18:19:15 +0800

    添加项目文件。

---
+++ OBPlayer.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace OpenBags;
+
+public class OBPlayer : ModPlayer
+{
+    public bool OpeningBags;
+
+    public override void ProcessTriggers(TriggersSet triggersSet)
+    {
+        if (OpenBags.OpenBagsKey.JustPressed)
+        {
+            ToggleOpenBags(byKeybind:true);
+        }
+    }
+
+    public override void PostUpdateMiscEffects()
+    {
+        if (OpeningBags)
+        {
+            foreach (var item in Player.inventory)
+            {
+                if (ItemLoader.CanRightClick(item) && Player.ConsumeItem(item.type))
+                {
+                    Player.DropFromItem(item.type);
+                    return;
+                }
+            }
+
+            ToggleOpenBags();
+        }
+    }
+
+    public void ToggleOpenBags(bool byKeybind = false)
+    {
+        string text;
+
+        if (byKeybind)
+            text = FromLocalization("OpenBags",
+                OpeningBags ? FromLocalization("Off") : FromLocalization("On"));
+        else
+            text = FromLocalization("AllBagsOpened");
+
+        CombatText.NewText(Player.Hitbox, Color.LightCyan, text);
+
+        OpeningBags = !OpeningBags;
+    }
+
+    public string FromLocalization(string key) => Language.GetTextValue($"Mods.OpenBags.{key}");
+    public string FromLocalization(string key, params object[] args) => Language.GetTextValue($"Mods.OpenBags.{key}", args);
+}   
---
+++ OpenBags.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Terraria.ModLoader;
+
+namespace OpenBags
+{
+	public class OpenBags : Mod
+    {
+        internal static ModKeybind OpenBagsKey;
+
+        public override void Load()
+        {
+            OpenBagsKey = KeybindLoader.RegisterKeybind(this, "OpenBags", Keys.O);
+        }
+
+        public override void Unload()
+        {
+            OpenBagsKey = null;
+        }
+    }
+}
